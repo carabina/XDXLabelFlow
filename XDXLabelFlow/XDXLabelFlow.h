c64dac68e083b41cadff808aba99b6314bc274ae
6谢侠6 <xiechong23@icloud.com>
2018-05-07 13:12:01 +0800

    2018年5月7日 13:15 Second Init Commit

---
+++ XDXLabelFlow/XDXLabelFlow.h
@@ -0,0 +1,19 @@
+//
+//  XDXLabelFlow.h
+//  XDXLabelFlow
+//
+//  Created by Chong Xie on 2018/5/7.
+//  Copyright © 2018年 6谢侠6 (6xieapplexia6). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XDXLabelFlow.
+FOUNDATION_EXPORT double XDXLabelFlowVersionNumber;
+
+//! Project version string for XDXLabelFlow.
+FOUNDATION_EXPORT const unsigned char XDXLabelFlowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XDXLabelFlow/PublicHeader.h>
+
+
---
+++ XDXLabelFlow/XDXLabelFlow.swift
@@ -0,0 +1,14 @@
+//
+//  XDXLabelFlow.swift
+//  XDXLabelFlow
+//
+//  Created by Chong Xie on 2018/5/7.
+//  Copyright © 2018年 6谢侠6 (6xieapplexia6). All rights reserved.
+//
+
+import UIKit
+
+public class XDXLabelFlow: UIView
+{
+    
+}
